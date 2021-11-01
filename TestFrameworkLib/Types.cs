278deb956f82f57537b9991ffc81dba461d49bf1
Alexandru Agape <alexandru.agape@student.tugraz.at>
2021-11-01 21:14:42 +0100

    Moved Utils class into its own file.

--- TestFrameworkLib/Types.cs
+++ TestFrameworkLib/Types.cs
@@ -1,7 +1,4 @@
-using System.Reflection;
-using System;
 using System.Collections.Generic;
-using NUnit.Framework;
 
 namespace TestFramework
 {
@@ -18,67 +15,6 @@ namespace TestFramework
     {
         Distance,
         Time
-    }
-
-    public static class Utils
-    {
-        public static bool IsValidHeader(string header_to_check)
-        {
-            var headers = GetHeaders();
-            foreach(var header in headers)
-            {
-                if(header_to_check == header)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public static List<string> GetHeaders()
-        {
-            List<string> headers = new List<string>();
-
-            Type type = typeof (ModFileHeader);
-            var fields = type.GetFields(BindingFlags.Static | BindingFlags.Public);
-
-            foreach (FieldInfo field in fields)
-            {
-                var fieldValue = (string)field.GetValue(null);
-                headers.Add(fieldValue);
-            }
-            return headers;
-        }
-
-        public static void ApplyOperator(double lhs, Operator op, double rhs, string errorMessage = "")
-        {
-            switch(op)
-            {
-                case Operator.Lower:
-                    Assert.That(lhs, Is.LessThan(rhs), errorMessage);
-                    break;
-                case Operator.Greater:
-                    Assert.That(lhs, Is.GreaterThan(rhs), errorMessage);
-                    break;
-                case Operator.Equals:
-                    Assert.That(lhs, Is.EqualTo(rhs), errorMessage);
-                    break;
-            }
-        }
-
-        public static string Symbol(Operator op, bool inverse=false)
-        {
-            switch(op)
-            {
-                case Operator.Lower:
-                    return inverse ? ">=" : "<";
-                case Operator.Greater:
-                    return inverse ? "<=" : ">";
-                case Operator.Equals:
-                    return inverse ? "!=" : "=";
-            }
-            return "";
-        }
     }
 
     public static class ModFileHeader
