4f9e66329ade5de8c6df9e1183d294e8c5c883b3
Michael Krisper <michael.krisper@tugraz.at>
2021-11-11 11:51:28 +0100

    changed path references to local paths, and set copy to output folder true for modfiles.

--- SampleTests/SampleUnitTests.cs
+++ SampleTests/SampleUnitTests.cs
@@ -1,4 +1,3 @@
-using System;
 using NUnit.Framework;
 using TestFramework;
 
@@ -8,7 +7,7 @@ namespace SampleUnitTests
     class UnitTests : VECTOTestFixture
     {
         [Test]
-        public void CityBus_AT_PS_RegionalDelivery_Cycle5() => VECTOTestCase("/home/alex/tugraz/VECTO/vecto-test-framework/SampleTests/mod_files/CityBus_AT_PS_RegionalDelivery.vmod",
+        public void CityBus_AT_PS_RegionalDelivery_Cycle5() => VECTOTestCase(@"mod_files\CityBus_AT_PS_RegionalDelivery.vmod",
             (0, 1e6, ModFileHeader.v_act, Operator.Greater, 500), 
             (0, 70, ModFileHeader.Gear, Operator.Lower, 4), // (0, 70, "gear", Operator.Greater, 4)
             (0, 70, ModFileHeader.P_eng_full_stat, Operator.Lower, 300) // (0, 70, "P_eng_full_stat", Operator.Equals, 250) -> if it's always the same value
@@ -18,7 +17,7 @@ namespace SampleUnitTests
         );
 
         [Test]
-        public void CityBus_AT_PS_RegionalDelivery_Cycle6() => VECTOTestCase("/home/alex/tugraz/VECTO/vecto-test-framework/SampleTests/mod_files/CityBus_AT_PS_RegionalDelivery.vmod",
+        public void CityBus_AT_PS_RegionalDelivery_Cycle6() => VECTOTestCase(@"mod_files\CityBus_AT_PS_RegionalDelivery.vmod",
             (0, 1e6, ModFileHeader.v_act, Operator.Lower, 90 /*SegmentType.Time*/), 
             (0, 1e6, ModFileHeader.Gear, Operator.Lower, 4 /*, SegmentType.Time*/)
         );
