cff0fa1b169f3ab343ac643ed707c17645eb380c
Alexandru Agape <alexandru.agape@student.tugraz.at>
2021-11-28 10:25:34 +0100

    fixed build errors

--- SampleTests/SampleUnitTests.cs
+++ SampleTests/SampleUnitTests.cs
@@ -7,18 +7,19 @@ namespace SampleUnitTests
     class UnitTests : VECTOTestFixture
     {
         [Test]
-        public void CityBus_AT_PS_RegionalDelivery_Cycle5() => VECTOTestCase(@"mod_files\CityBus_AT_PS_RegionalDelivery.vmod",
+        public void CityBus_AT_PS_RegionalDelivery_Cycle5() => VECTOTestCase(@"mod_files/CityBus_AT_PS_RegionalDelivery.vmod",
             (0, 1e6, ModFileHeader.v_act, Operator.Greater, 100), 
             (0, 1e6, ModFileHeader.v_act, Operator.Lower, 500),
-            (0, 70, ModFileHeader.Gear, Operator.Lower, 4), // (0, 70, "gear", Operator.Greater, 4)
-            ((0, /* tolerance*/5), (70, /* tolerance*/15), ModFileHeader.P_eng_full_stat, Operator.Lower, 300) // (0, 70, "P_eng_full_stat", Operator.Equals, 250) -> if it's always the same value
+            (0, 70, ModFileHeader.Gear, Operator.Lower, 4) // (0, 70, "gear", Operator.Greater, 4)
+            //((0, /* tolerance*/5), (70, /* tolerance*/15), ModFileHeader.P_eng_full_stat, Operator.Lower, 300) // (0, 70, "P_eng_full_stat", Operator.Equals, 250) -> if it's always the same value
                                                              // (0, 70, "P_eng_full_stat", Operator.Greater, min) -> if there are multiple values
                                                              // (0, 70, "P_eng_full_stat", Operator.Lower, max) 
             
         );
 
         [Test]
-        public void CityBus_AT_PS_RegionalDelivery_Cycle6() => VECTOTestCase(@"mod_files\CityBus_AT_PS_RegionalDelivery.vmod",
+        public void CityBus_AT_PS_RegionalDelivery_Cycle6() => VECTOTestCase(@"mod_files/CityBus_AT_PS_RegionalDelivery.vmod",
+            (0, 1e6, ModFileHeader.v_act, Operator.Greater, 100)
             // (50, 80, (ModFileHeader.v_act, ModFileHeader.Gear ...), Operator.Analyze) -> 
             // Ouput should be :
             //     (50, 60, ModFileHeader.v_act, Operator.Greater, 90),
